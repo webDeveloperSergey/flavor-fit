d5d6d6654ae29fae981ee7c65be54c4994b93c04
Sergei Kultyshkin <kultyshkin2003@gmail.com>
2026-02-10 17:28:04 +0300

    add(main): prettier + moduleFormat + ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
-import { PrismaModule } from './prisma/prisma.module.js';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller.js";
+import { AppService } from "./app.service.js";
+import { PrismaModule } from "./prisma/prisma.module.js";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
