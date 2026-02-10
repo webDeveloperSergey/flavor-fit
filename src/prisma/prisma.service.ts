58b2d4800bf67c2725749ad850ce08555eca134b
Sergei <kultyshkin2003@gmail.com>
2026-02-10 07:56:14 +0300

    init(main): начальный инит с backend (prisma + models)

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import {PrismaClient} from "../../prisma/generated/prisma/client.js";
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+    constructor() {
+        const adapter = new PrismaPg({
+            connectionString: process.env.DATABASE_URL as string,
+        });
+        super({ adapter });
+    }
+}
