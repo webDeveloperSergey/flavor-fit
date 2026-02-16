38ebd45c0448966241a31b9cf1d24a9072791929
Sergei <kultyshkin2003@gmail.com>
2026-02-16 18:58:37 +0300

    add(main): настройка конфига для graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller.js";
-import { AppService } from "./app.service.js";
-import { PrismaModule } from "./prisma/prisma.module.js";
-import { ConfigModule } from "@nestjs/config";
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller.js'
+import { AppService } from './app.service.js'
+import { PrismaModule } from './prisma/prisma.module.js'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { getGraphQLConfig } from './config/graphql.config.js'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      useFactory: getGraphQLConfig,
+      inject: [ConfigService],
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/graphql.config.ts
@@ -0,0 +1,15 @@
+import {ConfigService} from "@nestjs/config";
+import {ApolloDriverConfig} from "@nestjs/apollo";
+import {Request, Response} from "express";
+
+interface GraphQLContext {
+    req: Request
+    res: Response
+}
+
+export const getGraphQLConfig = (configService: ConfigService): ApolloDriverConfig => ({
+    autoSchemaFile: true,
+    playground: configService.get('MODE') === 'development',
+    sortSchema: true,
+    context: ({ req, res }: GraphQLContext): GraphQLContext => ({req, res })
+})
